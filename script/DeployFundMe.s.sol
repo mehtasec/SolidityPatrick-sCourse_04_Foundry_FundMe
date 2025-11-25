91491650bc89710969668eac359715d8165841c2
ujjwal <h6ck+1@wearehackerone.com>
2025-11-25 23:54:50 +0530

    11. deploy mock price feed and helperConfig.s.sol

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.30;
 
 import {FundMe} from "../src/FundMe.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) { // shouldn't it return fundMe?
+        // before startBroadcast -> not a real "tx" , just simulate
+        HelperConfig helperConfig = new HelperConfig();
+        (address ethUsdPriceFeed) = helperConfig.activeNetworkConfig(); // if we had multiple return values in struct, seperate by comma
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe ;
     }
