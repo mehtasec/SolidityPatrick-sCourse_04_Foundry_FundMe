dd2d4b5044dbff9e82d4fcad68a11d67e9a1ee1c
ujjwal <h6ck+1@wearehackerone.com>
2025-11-25 21:21:27 +0530

    9. running test on chain forks

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,15 +11,20 @@ contract FundMeTest is Test {
         fundMe = new FundMe();
     }
 
-    function testMinimumDollarIsFive() public {
+    function testMinimumDollarIsFive() public view{
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 
-    function testOwnerIsMsgSender() public {
+    function testOwnerIsMsgSender() public view{
         // assertEq(fundMe.i_owner(), msg.sender); // us calling -> FundMeTest -> this contract deploys new isntance of FundMe, se address is owner here.
         assertEq(fundMe.i_owner(), address(this));
         
         
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion(); // while calling getVersion, use sepolia rpc_url with chain-fork(--fork-url)
+        assertEq(version, 4); 
     }
     
 }
