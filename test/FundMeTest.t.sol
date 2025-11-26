0b94486844191002dda6768069621e49dee23d93
ujjwal <h6ck+1@wearehackerone.com>
2025-11-26 23:52:43 +0530

    16. foundry test cheatcode

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,11 +7,13 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306); // refactor
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, 10e18);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -27,5 +29,18 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion(); // while calling getVersion, use sepolia rpc_url with chain-fork(--fork-url)
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // means, the next line should revert
+        // assert (this tx fails/reverts )
+        fundMe.fund();  // how we're able to use fundMe here w/o creating new instance, fundMe FundMe works, but importing doesnt work  
+    }
+
+    function testFundUpdatesFundedDataStructure() public {  // check if funds are updating
+        vm.prank(USER); // next tx will be sent by USER, since who sends what gets confusing
+        fundMe.fund{value: 10e18}(); 
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER); // instead of address(this) or msg.sender, use USER 
+        assertEq(amountFunded, 10e18);
     }
 }
